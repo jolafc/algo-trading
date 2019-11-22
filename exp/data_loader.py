8a348f3a3c05dabcd903e9c61bd6cb4cc797cd4f
Jonathan Laflamme Janssen <jonathanl@stradigi.ai>
2019-11-22 15:02:59 -0500

    Rudimentary data cleaning. Can extract any feature/column from the data dict.

--- exp/data_loader.py
+++ exp/data_loader.py
@@ -7,36 +7,65 @@ from exp import DATA_DIR, PLT_FILE_FORMAT, SP500_PKL
 from exp.data_getter import load_prices_dict
 
 ADJUSTED_CLOSE_COLUMN = 'adjusted_close'
+OPEN_COLUMN = 'open'
+HIGH_COLUMN = 'high'
+LOW_COLUMN = 'low'
+CLOSE_COLUMN = 'close'
+VOLUME_COLUMN = 'volume'
+DIVIDENT_COLUMN = 'dividend_amount'
+SPLIT_COLUMN = 'split_coefficient'
 
-def get_close_prices(pkl_file=SP500_PKL, debug=False):
-    prices_dict = load_prices_dict(pkl_file=pkl_file)
-    prices_dict = {k: v for k, v in prices_dict.items() if v is not None}
 
-    adj_close_prices = [df[ADJUSTED_CLOSE_COLUMN].rename(k) for k, df in prices_dict.items()]
-    adj_close_prices = pd.concat(adj_close_prices, axis=1)
+### TODO: Need the joiner and leaver data of the SP500 stocks and filtering functions
+
+def get_feature(prices_dict, column=ADJUSTED_CLOSE_COLUMN, debug=False, impute=None):
+    prices_dict_clean = {k: v for k, v in prices_dict.items() if v is not None}
+
+    feature_df = [df[column].rename(k) for k, df in prices_dict_clean.items()]
+    feature_df = pd.concat(feature_df, axis=1)
 
     if debug:
-        n_nans = adj_close_prices.isna().sum(axis=0).sort_values(ascending=False)
-        print(f'Number of Nans per ticker:\n{n_nans}\n')
+        report_nans(feature_df, feature_name=column)
+    if impute is not None:
+        feature_df = impute_time_series(feature_df, method=impute)
 
-        adj_close_prices.isna().sum(axis=1).plot()
-        plotfile = os.path.join(DATA_DIR, f'N_nans_in_time.{PLT_FILE_FORMAT}')
-        plt.savefig(plotfile)
-        print(f'Number of Nans per day plotted in file: {plotfile}\n')
-
-    return adj_close_prices
+    return feature_df
 
 
-def get_rolling_window(df, end, length):
-    rolling_window = adj_close_prices[:end].iloc[-length:]
+def report_nans(df, feature_name=ADJUSTED_CLOSE_COLUMN):
+    n_nans = df.isna().sum(axis=0).sort_values(ascending=False)
+    print(f'Number of Nans per ticker for {feature_name}:\n{n_nans}\n')
+
+    df.isna().sum(axis=1).plot()
+    plotfile = os.path.join(DATA_DIR, f'Nans_{feature_name}.{PLT_FILE_FORMAT}')
+    plt.savefig(plotfile)
+    print(f'Number of Nans per day for {feature_name} plotted in file: {plotfile}\n')
+
+
+def impute_time_series(df, method=None):
+    if method == 'pad':
+        df = df.fillna(method=method)
+    return df
+
+
+def get_rolling_window(df, end, length, dropna=True):
+    rolling_window = df[:end].iloc[-length:]
+    if dropna:
+        rolling_window = rolling_window.dropna(axis=1)
 
     return rolling_window
 
 
 if __name__ == '__main__':
-    adj_close_prices = get_close_prices(debug=True)
+    pkl_file = SP500_PKL  # par
+    prices_dict = load_prices_dict(pkl_file=pkl_file)
 
-    end = pd.to_datetime('2019-01-02')
-    length = 200
-    rolling_window = get_rolling_window(adj_close_prices, end=end, length=length)
-    print(rolling_window)
+    adj_close_prices = get_feature(prices_dict, column=ADJUSTED_CLOSE_COLUMN, debug=False, impute=False)
+    volumes = get_feature(prices_dict, column=VOLUME_COLUMN, debug=False, impute=False)
+
+    end = pd.to_datetime('2019-01-02')  # par
+    length = 200  # par
+    prices_window = get_rolling_window(adj_close_prices, end=end, length=length, dropna=True)
+    volumes_window = get_rolling_window(volumes, end=end, length=length, dropna=True)
+
+    print(prices_window.shape, volumes_window.shape)
