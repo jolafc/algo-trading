e341b8c7a2295028eb018cedd38a5c8e2984afc9
Jonathan Laflamme Janssen <jonathanl@stradigi.ai>
2019-11-22 21:33:53 -0500

    First draft of the determination of the portfolio constitution for a random date according to the "Weeklu rotation S&P 500"

--- exp/data_loader.py
+++ exp/data_loader.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt
 
+import ta.momentum
+
 from exp import DATA_DIR, PLT_FILE_FORMAT, SP500_PKL
 from exp.data_getter import load_prices_dict
 
@@ -48,8 +50,10 @@ def impute_time_series(df, method=None):
     return df
 
 
-def get_rolling_window(df, end, length, dropna=True):
-    rolling_window = df[:end].iloc[-length:]
+def get_rolling_window(df, date, lookback=None, dropna=True):
+    rolling_window = df[:date]
+    if lookback is not None:
+        rolling_window = rolling_window.iloc[-lookback:]
     if dropna:
         rolling_window = rolling_window.dropna(axis=1)
 
@@ -57,15 +61,56 @@ def get_rolling_window(df, end, length, dropna=True):
 
 
 if __name__ == '__main__':
-    pkl_file = SP500_PKL  # par
+    pkl_file = SP500_PKL  # PAR
     prices_dict = load_prices_dict(pkl_file=pkl_file)
 
     adj_close_prices = get_feature(prices_dict, column=ADJUSTED_CLOSE_COLUMN, debug=False, impute=False)
     volumes = get_feature(prices_dict, column=VOLUME_COLUMN, debug=False, impute=False)
+    rsi_lookback = 3 # PAR
+    rsis = adj_close_prices.apply(lambda x: ta.momentum.rsi(x, n=rsi_lookback))
 
-    end = pd.to_datetime('2019-01-02')  # par
-    length = 200  # par
-    prices_window = get_rolling_window(adj_close_prices, end=end, length=length, dropna=True)
-    volumes_window = get_rolling_window(volumes, end=end, length=length, dropna=True)
+    date = pd.to_datetime('2019-01-31')  # PAR
+    lookback = 200  # PAR
+    prices_window = get_rolling_window(adj_close_prices, date=date, lookback=lookback, dropna=True)
+    volumes_window = get_rolling_window(volumes, date=date, lookback=lookback, dropna=True)
 
-    print(prices_window.shape, volumes_window.shape)
+    # print(prices_window.shape, volumes_window.shape)
+
+    # Filters for §8: Weekly rotation of the S&P 500 - The 30-Minute Stock Trader
+    # 1 - SPY is above 0.98*SMA(200)
+    spy_series = adj_close_prices['SPY']
+    spy_window = get_rolling_window(spy_series, date=date, lookback=lookback, dropna=False)
+    spy_sma = spy_window.mean()
+    spy_today = spy_series[date]
+    sma_tol = 0.02 # PAR
+    spy_condition = spy_today > spy_sma * (1 - sma_tol)
+    print(f'SPY condition to trade: {spy_today} > {spy_sma} to {sma_tol*100:.0f}% ==> {spy_condition}')
+    if spy_condition:
+
+        # 2 - avg. vol. (20) >= 1M
+        volume_lookback = 20 # PAR
+        volumes_window = get_rolling_window(volumes, date=date, lookback=volume_lookback, dropna=False)
+        volumes_sma = volumes_window.mean(axis=0)
+        volume_threshold = 1e6 # PAR
+        volumes_mask = volumes_sma >= volume_threshold
+        print(f'Volume filtering: {volumes_mask.sum()} / {volumes_mask.shape} passed.')
+
+        # 3 - Min price 1$
+        price_threshold = 1. # PAR
+        price_mask = adj_close_prices.loc[date, :] > price_threshold
+        print(f'Price filtering: {price_mask.sum()} / {price_mask.shape} passed.')
+
+        # 4 - RSI(3) < 50
+        rsi_threshold = 50. # PAR
+        rsi_mask = rsis.loc[date, :] < rsi_threshold
+        print(f'RSI filtering: {rsi_mask.sum()} / {rsi_mask.shape} passed.')
+
+        # Positions - sorted(ROC(200), descending), Update positions to 0.1 for first 10 elements of the sorted list
+        mask = volumes_mask & price_mask & rsi_mask
+        print(f'Filtering: {mask.sum()} / {mask.shape} passed.')
+        prices_window = get_rolling_window(adj_close_prices, date=date, lookback=lookback, dropna=False)
+        roc = prices_window.iloc[-1, :] / prices_window.iloc[0, :]
+        n_positions = 10
+        roc_sorted = roc[mask].sort_values(ascending=False)
+        positions = list(roc_sorted[:n_positions].index)
+        print(positions)
