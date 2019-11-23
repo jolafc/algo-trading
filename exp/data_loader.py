69b256b6b80cc29bae61d251186ff340e78201b2
Jonathan Laflamme Janssen <jonathanl@stradigi.ai>
2019-11-23 10:05:30 -0500

    Embryonic backtesting loop with position computation with no trades/portfolio yet. Vectorized operations; only things from filtering of stocks onwards occurs in the (backtesting) loop.

--- exp/data_loader.py
+++ exp/data_loader.py
@@ -28,7 +28,7 @@ def get_feature(prices_dict, column=ADJUSTED_CLOSE_COLUMN, start_idx=None, end_i
     feature_df = [df[column].rename(k) for k, df in prices_dict_clean.items()]
     feature_df = pd.concat(feature_df, axis=1)
     if start_idx is not None and end_idx is not None:
-        feature_df = feature_df.iloc[start_idx:end_idx+1, :]
+        feature_df = feature_df.iloc[start_idx:end_idx + 1, :]
 
     if debug:
         report_nans(feature_df, feature_name=column)
@@ -54,45 +54,36 @@ def impute_time_series(df, method=None):
     return df
 
 
-def get_rolling_window(df, date, lookback=None, dropna=True):
-    rolling_window = df[:date]
-    if lookback is not None:
-        rolling_window = rolling_window.iloc[-lookback:]
-    if dropna:
-        rolling_window = rolling_window.dropna(axis=1)
-
-    return rolling_window
-
-
 if __name__ == '__main__':
-    pkl_file = SP500_PKL  # PAR
+    pkl_file = SP500_PKL  # PAR data
 
-    start_date = pd.to_datetime('2019-01-31')  # PAR
-    end_date = pd.to_datetime('2019-10-31')  # PAR
-    lookback = 200  # PAR
+    start_date = pd.to_datetime('2019-01-31')  # PAR backtesting
+    end_date = pd.to_datetime('2019-10-31')  # PAR backtesting
+    lookback = 200  # PAR backtesting
 
-    rsi_lookback = 3 # PAR
-    date = pd.to_datetime('2019-01-31')  # PAR
-    sma_tol = 0.02 # PAR
-    volume_lookback = 20 # PAR
-    volume_threshold = 1e6 # PAR
-    price_threshold = 1. # PAR
-    rsi_threshold = 50. # PAR
-    n_positions = 10 # PAR
+    sma_tol = 0.02  # PAR strategy
+    volume_lookback = 20  # PAR strategy
+    volume_threshold = 1e6  # PAR strategy
+    price_threshold = 1.  # PAR strategy
+    rsi_lookback = 3  # PAR strategy
+    rsi_threshold = 50.  # PAR strategy
+    day_of_trade = 4  # PAR strategy - pandas DatetimeIndex.dayofweek value for Friday
+    n_positions = 10  # PAR strategy
 
     prices_dict = load_prices_dict(pkl_file=pkl_file)
     adj_close_prices = get_feature(prices_dict, column=ADJUSTED_CLOSE_COLUMN, debug=False, impute=False)
     start_idx = adj_close_prices.index.get_loc(start_date) - (lookback - 1)
     end_idx = adj_close_prices.index.get_loc(end_date)
-    adj_close_prices = adj_close_prices.iloc[start_idx:end_idx+1]
+    adj_close_prices = adj_close_prices.iloc[start_idx:end_idx + 1]
 
-    volumes = get_feature(prices_dict, column=VOLUME_COLUMN, start_idx=start_idx, end_idx=end_idx, debug=False, impute=False)
+    volumes = get_feature(prices_dict, column=VOLUME_COLUMN, start_idx=start_idx, end_idx=end_idx, debug=False,
+                          impute=False)
 
     # Filters for §8: Weekly rotation of the S&P 500 - The 30-Minute Stock Trader
     # 1 - SPY is above 0.98*SMA(200)
     spy = adj_close_prices['SPY']
     spy_sma = adj_close_prices['SPY'].rolling(lookback).mean()
-    spy_conditions = spy > spy_sma * (1 - sma_tol)
+    spy_masks = spy > spy_sma * (1 - sma_tol)
 
     # 2 - avg. vol. (20) >= 1M
     volumes_sma = volumes.rolling(volume_lookback).mean()
@@ -105,25 +96,23 @@ if __name__ == '__main__':
     prices_rsi = adj_close_prices.apply(lambda x: ta.momentum.rsi(x, n=rsi_lookback))
     rsi_masks = prices_rsi < rsi_threshold
 
-    # 5 - Sort wrt the ROC(200)
-    rocs = adj_close_prices / adj_close_prices.shift(lookback-1)
+    # Total masks 1-4
+    # For pd.Series: .values[:, None] is to transform the series to a np.array and use its broadcasting capabilities,
+    # specifying which axis needs to be repeated
+    masks = volumes_masks & price_masks & rsi_masks & spy_masks.values[:, None]
 
-    print(f'SPY condition to trade: {spy[date]} > {spy_sma[date]} to {sma_tol*100:.0f}% ==> {spy_conditions[date]}')
-    if spy_conditions[date]:
-        volumes_mask = volumes_masks.loc[date, :]
-        print(f'Volume filtering: {volumes_mask.sum()} / {volumes_mask.shape} passed.')
+    # 5a - Sort wrt the ROC(200)
+    rocs = adj_close_prices / adj_close_prices.shift(lookback - 1)
 
-        price_mask = price_masks.loc[date, :]
-        print(f'Price filtering: {price_mask.sum()} / {price_mask.shape} passed.')
+    # 5b - trade on Fridays in the backtesting window
+    valid_dates = adj_close_prices.index
+    day_of_week_triggers = valid_dates.dayofweek == day_of_trade
+    backtesting_window_triggers = (valid_dates >= start_date) & (valid_dates <= end_date)
+    triggers = day_of_week_triggers & backtesting_window_triggers
+    dates = valid_dates[triggers]
 
-        rsi_mask = rsi_masks.loc[date, :]
-        print(f'RSI filtering: {rsi_mask.sum()} / {rsi_mask.shape} passed.')
-
-        # Positions - sorted(ROC(200), descending), Update positions to 0.1 for first 10 elements of the sorted list
-        mask = volumes_mask & price_mask & rsi_mask
-        print(f'Filtering: {mask.sum()} / {mask.shape} passed.')
-
-        roc = rocs.loc[date, :]
-        roc_sorted = roc[mask].sort_values(ascending=False)
-        positions = list(roc_sorted[:n_positions].index)
-        print(positions)
+    for date in dates:
+        mask = masks.loc[date]
+        roc = rocs.loc[date]
+        positions = roc[mask].sort_values(ascending=False)[:n_positions].index.tolist()
+        print(f'{date.date()} - Positions: {positions}')
